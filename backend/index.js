e4922a4975a7164539134a3296b03ae6c31e9c12
Camara Hadibéré <hadibere@gmail.com>
2019-12-19 15:28:56 +0100

    correction v2 key ok

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ const app = express();
 const PORT = 5000;
 const bodyParser = require("body-parser");
 const sequelize = require("./database/config/connect");
+const secrets = require("./secrets");
+const User = require("./database/models/").User;
+const Token = require("./database/models/").Token;
+const cors = require("cors");
+
+app.use(cors());
+app.options("*", cors());
 
 sequelize
 	.authenticate()
@@ -19,5 +26,49 @@ sequelize
 app.get("/", (req, res) => {
 	res.send("Hello Back !");
 });
+
+app.get("/getNewToken", async (req, res) => {
+	Token.findOne({}).then(async (tokenCreate) => {
+		if (!tokenCreate) {
+			Token.create({
+				token: await getNewToken(),
+			});
+		}
+
+		if (tokenCreate && tokenCreate.createdAt) {
+			const result = Math.round(
+				(Date.now() - Date.parse(tokenCreate.createdAt)) / 1000,
+			);
+			console.log(result);
+			if (result >= 200) {
+				Token.destroy({
+					where: {},
+				});
+			}
+		}
+	});
+	res.sendStatus(200);
+});
+
+const getNewToken = async () => {
+	const token = await axios
+		.post(
+			"https://auth.maas-dev.aws.vsct.fr/oauth2/token",
+			"grant_type=client_credentials&scope=https%3A%2F%2Fapi.maas-dev.aws.vsct.fr%2F.*%2Fsearch.*%3A.*",
+			{
+				headers: {
+					Authorization: secrets.auth,
+					"Content-Type": "application/x-www-form-urlencoded",
+					"x-api-key": secrets.apiKey,
+				},
+			},
+		)
+		.then((res) => {
+			return res.data.access_token;
+		})
+		.catch((err) => console.log(err.message));
+
+	return token;
+};
 
 app.listen(PORT, console.log(`Ecoute sur le port ${PORT}`));
