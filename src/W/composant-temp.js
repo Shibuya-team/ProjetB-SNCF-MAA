217a8739055031fd3e127b50b571a7e87649467d
Camara Hadibéré <hadibere@gmail.com>
2019-12-19 13:37:28 +0100

    first commit travis

---
+++ src/W/composant-temp.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export const Home = () => {
+  return <p>Index - home</p>;
+};
+
+export const PageUn = () => {
+  return <p>Page 1</p>;
+};
---
+++ src/components/FormTravel/Departure.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Departure() {
+	return <></>;
+}
+
+export default Departure;
