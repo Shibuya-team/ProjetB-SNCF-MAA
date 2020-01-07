e4bd190876b90f1169fa4521a0a42f46c6e9707d
AudCor <clapyourhands4ever@gmail.com>
2020-01-07 16:37:36 +0100

    [WIP] Route recherche itinéraire

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const util = require("util");
 const app = express();
 const PORT = 5000;
 // const bodyParser = require("body-parser");
@@ -20,14 +21,19 @@ sequelize
     );
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello Back !");
-});
+// app.get("/token", async (req, res) => {
+//   const token = await getNewToken();
 
-const getNewToken = () => {
+//   console.log(token);
+//   res.send(token);
+// });
+
+app.listen(PORT, console.log(`Ecoute sur le port ${PORT}`));
+
+const getNewToken = async () => {
   let token = "";
   let expires_time = 0;
-  axios
+  await axios
     .post(
       "https://auth.maas-dev.aws.vsct.fr/oauth2/token",
       "grant_type=client_credentials&scope=https%3A%2F%2Fapi.maas-dev.aws.vsct.fr%2F.*%2Fsearch.*%3A.*",
@@ -42,88 +48,135 @@ const getNewToken = () => {
     .then(res => {
       token = res.data.access_token;
       expires_time = Math.floor(Date.now() / 1000) + res.data.expires_in;
-      console.log("token : " + token);
-      console.log("enregistré à :" + expires_time);
+      // console.log("token : " + token);
+      // console.log("enregistré à :" + expires_time);
       // envoyer ces deux variables dans stockage données : champs : "token" et "token_created_time"
     })
-    .catch(err => console.log(err.message));
+    .catch(err => {
+      console.log(err.message);
+    });
+
   return token;
 };
 
-// fonction appelée à chaque fois que le client valide une recherche "itinéraire" ou "around me", pour récupérer son token ou en générer un nouveau
-// const getToken = () => {
-//   let token = User.findAll({ where: { id: mock.userId } }).then(user => {}); // récupérer champ "token" dans la table "user"
-//   let expires_time = User.findAll({}); // récupérer champ "token_created_time" dans la table "user"
-//   if (token === "" || token === null || !token) {
-//     getNewToken(token, expires_time);
-//   } else {
-//     const dateNow = Math.floor(Date.now() / 1000);
-//     if (dateNow - expires_time >= 0) {
-//       getNewToken(token, expires_time);
-//     }
-//   }
-//   return token;
-// };
+// // fonction appelée à chaque fois que le client valide une recherche "itinéraire" ou "around me", pour récupérer son token ou en générer un nouveau
+// // const getToken = () => {
+// //   let token = User.findAll({ where: { id: mock.userId } }).then(user => {}); // récupérer champ "token" dans la table "user"
+// //   let expires_time = User.findAll({}); // récupérer champ "token_created_time" dans la table "user"
+// //   if (token === "" || token === null || !token) {
+// //     getNewToken(token, expires_time);
+// //   } else {
+// //     const dateNow = Math.floor(Date.now() / 1000);
+// //     if (dateNow - expires_time >= 0) {
+// //       getNewToken(token, expires_time);
+// //     }
+// //   }
+// //   return token;
+// // };
 
-// à compléter
+// à compléter : fonction de validation de la recherche
+
 const search = async () => {
-  const token = await getNewToken();
-  console.log("token 2:" + token);
-  let searchId = "";
-  // let resItinerary = {};
-  // Obtention des résultats
-  app.get("/itinerary/search/", (req, res) => {
-    async function getSearchId() {
-      return await axios
-        .post(
-          "https://api.maas-dev.aws.vsct.fr/master/search/itinerary",
-          { headers: `Bearer ${token}` },
-          {
-            body: {
-              destination: {
-                latitude: 48.9595466,
-                longitude: 2.3424024
-              },
-              mobilityTypes: ["VEHICLE_WITH_DRIVER"],
-              origin: {
-                latitude: 48.8534,
-                longitude: 2.3488
-              },
-              partners: [""],
-              searchDate: "2020-01-03T19:54:20.026Z"
-            }
+  let searchId = null;
+  let resItinerary = {};
+
+  const newtoken = await getNewToken();
+
+  const getSearchId = async () => {
+    console.log("getSearchId newtoken : ", newtoken);
+
+    // "https://api.maas-dev.aws.vsct.fr/enc/search/itinerary" -H  "accept: application/json" -H  "Content-Type: application/json" -H "Authorization: Bearer " -d "{\"destination\":{\"latitude\":0,\"longitude\":0},\"mobilityTypes\":[\"VEHICLE_WITH_DRIVER\"],\"origin\":{\"latitude\":0,\"longitude\":0},\"partners\":[\"string\"],\"searchDate\":\"2020-01-09T13:54:20.026Z\"}"
+    await axios
+      .post(
+        "https://api.maas-dev.aws.vsct.fr/enc/search/itinerary",
+
+        {
+          destination: {
+            latitude: 48.9595466,
+            longitude: 2.3424024
+          },
+          origin: {
+            latitude: 48.8534,
+            longitude: 2.3488
           }
-        )
-        .then(res => {
-          searchId = res.data.searchId;
-          console.log("3: " + searchId);
-          res.send(searchId);
-        })
-        .catch(err => console.log(err.message));
-    }
+        },
+        {
+          headers: {
+            // accept: "application/json",
+            Authorization: `Bearer ${newtoken}`,
+            "x-api-key": secrets.apiKey,
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        }
+      )
+      .then(res => {
+        searchId = res.data.searchId;
+        console.log("OK ! searchId : " + searchId);
+      })
+      .catch(err => {
+        console.log(
+          "Échec search hjgjgj! " + util.inspect(err.response.config)
+        );
+      });
+  };
 
-    // function getItineraryResults() {
-    //   return axios
-    //     .get(
-    //       "https://api.maas-dev.aws.vsct.fr/master/search/itinerary/{searchId}"
-    //     )
-    //     .then(res => {
-    //       resItinerary = res.data;
-    //       res.send("Votre itinéraire!");
-    //     })
-    //     .catch(err => console.log(err.message));
-    // }
-
-    // axios
-    //   .all([getSearchId(), getItineraryResults()])
-    //   .then(axios.spread(function(searchId, itinerary) {}));
-
-    getSearchId();
-    console.log("4 : " + getSearchId());
-    return searchId;
-  });
+  const getItineraryResults = async () => {
+    await getSearchId();
+    console.log("search", searchId);
+    return await axios
+      .get(
+        `https://api.maas-dev.aws.vsct.fr/enc/search/itinerary/${searchId}`,
+        {
+          headers: `Bearer ${newtoken}`
+        }
+      )
+      .then(res => {
+        resItinerary = res.data;
+        console.log("resItinerary : " + resItinerary);
+      })
+      .catch(err => {
+        console.log("Échec resItinerary ! " + err);
+      });
+  };
+  return await getItineraryResults();
 };
 
-search();
+app.get("/search", async (req, response) => {
+  const resItinerary = await search();
+  console.log(resItinerary);
+  response.send(resItinerary);
+});
 
-app.listen(PORT, console.log(`Ecoute sur le port ${PORT}`));
+app.get("/test", async (req, res) => {
+  const token = await getNewToken;
+
+  axios
+    .post(
+      "https://api.maas-dev.aws.vsct.fr/enc/search/itinerary",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "x-api-key": "IxcoQOJ2xe4sqEl9bwKpH1EwxPPJhTLc1xxvEgjp",
+          "Content-Type": "application/json"
+        }
+      },
+      {
+        destination: {
+          latitude: 48.9595466,
+          longitude: 2.3424024
+        },
+        origin: {
+          latitude: 48.8534,
+          longitude: 2.3488
+        }
+      }
+    )
+    .then(result => {
+      console.log(res);
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
+    });
+});
