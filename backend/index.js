9b33798cd3f42d80173480188c95cd11a2df92f1
Camara Hadibéré <hadibere@gmail.com>
2020-01-13 12:05:49 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -5,72 +5,108 @@ const PORT = 5000;
 const bodyParser = require("body-parser");
 const sequelize = require("./database/config/connect");
 const secrets = require("./secrets");
-const mock = require("./mockData");
 const User = require("./database/models/").User;
+const Token = require("./database/models/").Token;
+const cors = require("cors");
+
+app.use(cors());
+app.options("*", cors());
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("La connexion a été établie avec succès.");
-  })
-  .catch(err => {
-    console.error(
-      "Impossible de se connecter à la base de données :",
-      err.message
-    );
-  });
+	.authenticate()
+	.then(() => {
+		console.log("La connexion a été établie avec succès.");
+	})
+	.catch((err) => {
+		console.error(
+			"Impossible de se connecter à la base de données :",
+			err.message,
+		);
+	});
 
 app.get("/", (req, res) => {
-  res.send("Hello Back !");
+	res.send("Hello Back !");
 });
 
-const getNewToken = (token, expires_time) => {
-  axios
-    .post(
-      "https://auth.maas-dev.aws.vsct.fr/oauth2/token",
-      "grant_type=client_credentials&scope=https%3A%2F%2Fapi.maas-dev.aws.vsct.fr%2F.*%2Fsearch.*%3A.*",
-      {
-        headers: {
-          Authorization: secrets.auth,
-          "Content-Type": "application/x-www-form-urlencoded",
-          "x-api-key": secrets.apiKey
-        }
-      }
-    )
-    .then(res => {
-      token = res.data.access_token;
-      expires_time = Math.floor(Date.now()) / 1000 + res.data.expires_in;
-      // envoyer ces deux variables dans stockage données : champs : "token" et "token_created_time"
+app.get("/user", (req, res) => {
+	User.create({
+		firstName: "hadibéré",
+		lastName: "CAMARA",
+		email: "hadibere@gmail.com",
+	});
+	res.send("User ajoutée");
+});
 
-      });
-    })
-    .catch(err => console.log(err.message));
+app.get("/getNewToken", async (req, res) => {
+	Token.findAll().then(async () => {
+		Token.findOrCreate({
+			where: {
+				token: await getNewToken(),
+			},
+		});
+	});
+	res.sendStatus(200);
+});
+
+app.get("/api", (req, res) => {
+	getNewToken();
+	res.sendStatus(200);
+});
+
+// Supression TEST //
+app.get("/delete", (req, res) => {
+	User.destroy({
+		where: {},
+	});
+	res.send("Delete");
+});
+
+app.get("/tokend", (req, res) => {
+	Token.destroy({
+		where: {},
+	});
+	res.send("Delete");
+});
+
+const getNewToken = async () => {
+	const token = await axios
+		.post(
+			"https://auth.maas-dev.aws.vsct.fr/oauth2/token",
+			"grant_type=client_credentials&scope=https%3A%2F%2Fapi.maas-dev.aws.vsct.fr%2F.*%2Fsearch.*%3A.*",
+			{
+				headers: {
+					Authorization: secrets.auth,
+					"Content-Type": "application/x-www-form-urlencoded",
+					"x-api-key": secrets.apiKey,
+				},
+			},
+		)
+		.then((res) => {
+			const token = res.data.access_token;
+
+			return token;
+			//expires_time = Math.floor(Date.now() / 1000) + res.data.expires_in;
+
+			// envoyer ces deux variables dans stockage données : champs : "token" et "token_created_time"
+		})
+		.catch((err) => console.log(err.message));
+
+	return token;
 };
 
 // fonction appelée à chaque fois que le client valide une recherche "itinéraire" ou "around me", pour récupérer son token ou en générer un nouveau
 const getToken = () => {
-  let token = User.findAll({where : {id: mock.userId}}).then(user => ); // récupérer champ "token" dans la table "user"
-  let expires_time = User.findAll(......); // récupérer champ "token_created_time" dans la table "user"
-  if (token === "" || token === null || !token) {
-    getNewToken(token, expires_time);
-  } else {
-    const dateNow = Math.floor(Date.now()) / 1000;
-    if (dateNow - expires_time >= 0) {
-      getNewToken(token, expires_time);
-    }
-  }
-  return token;
-};
-
-// à compléter
-const search = () => {
-  const token = getToken();
-  axios
-    .post()
-    .then(res => {
-      const results = res.data;
-    })
-    .catch(err => console.log(err.message));
+	//let token = User.findAll({ where: { id: mock.userId } }).then((user) => {}); // récupérer champ "token" dans la table "user"
+	let expires_time = User.findAll({}); // récupérer champ "token_created_time" dans la table "user"
+	if (token === "" || token === null || !token) {
+		getNewToken(token, expires_time);
+	} else {
+		const dateNow = Math.floor(Date.now() / 1000);
+		if (dateNow - expires_time >= 0) {
+			getNewToken(token, expires_time);
+		}
+	}
+	return token;
 };
 
 app.listen(PORT, console.log(`Ecoute sur le port ${PORT}`));
