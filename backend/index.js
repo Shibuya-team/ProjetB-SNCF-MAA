73a17796090c781f3f8868c0864445a930606b2b
Camara Hadibéré <hadibere@gmail.com>
2020-01-23 09:24:43 +0100

    ajout du require axios

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 const PORT = 5000;
 const bodyParser = require("body-parser");
 const sequelize = require("./database/config/connect");
