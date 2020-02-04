edead2500939694167beb4f36e0c1d2c0c0c5180
Camara Hadibéré <hadibere@gmail.com>
2020-02-04 11:43:00 +0100

    color correction

--- frontend/src/components/color.js
+++ frontend/src/components/color.js
@@ -1,21 +1,4 @@
 const color = {
-<<<<<<<<< Temporary merge branch 1
-	grey: "#EBE8E8",
-	oldpink: "#F0DBD8",
-	salmon: "#FFCAB8",
-	gold: "#F9C153",
-	purple: "#241F5D",
-	velvet: "#5353A2",
-	plum: "#6767AD",
-	emerald: "#78C4D2",
-	turquoise: "#7ED0DF",
-	asura: "#94DAD5",
-	orange: "#FF8B66",
-	brick: "#EB5933",
-	white: "#FFFFFF",
-};
-export default color;
-=========
     grey: "#EBE8E8",
     oldpink: "#F0DBD8",
     salmon: "#FFCAB8",
@@ -31,4 +14,4 @@ export default color;
     white: "#FFFFFF",
 };
 export default color;
->>>>>>>>> Temporary merge branch 2
+
