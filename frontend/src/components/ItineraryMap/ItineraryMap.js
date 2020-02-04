5027ff7f08d227810c809f7f49d6cf40555f80eb
Camara Hadibéré <hadibere@gmail.com>
2020-02-04 07:55:47 +0100

    supression de fichier inutile

--- frontend/src/components/ItineraryMap/ItineraryMap.js
+++ frontend/src/components/ItineraryMap/ItineraryMap.js
@@ -1,163 +1,107 @@
-import {
-  GoogleMapProvider,
-  MapBox,
-  Marker,
-  Polyline,
-  OverlayView,
-  InfoWindow,
-  StandaloneAutocomplete
-} from "@googlemap-react/core";
 import React from "react";
+import {
+	GoogleMapProvider,
+	MapBox,
+	Marker,
+	Polyline,
+} from "@googlemap-react/core";
 import Taxi from "../../images/icones/Mapicones/Taxi";
 import CoDriving from "../../images/icones/Mapicones/CoDriving";
-import styled from "styled-components";
 import secrets from "../../secrets";
-import ButtonStyle from "../ButtonStyle";
-import Datepicker from "../FormTravel/DatePicker";
-import CardList from "../FormTravel/CardList";
-
+import useGlobal from "../../global-state-management/store";
 
 const color = {
-  grey: "#EBE8E8",
-  oldpink: "#F0DBD8",
-  salmon: "#FFCAB8",
-  gold: "#F9C153",
-  purple: "#241F5D",
-  velvet: "#5353A2",
-  plum: "#6767AD",
-  emerald: "#78C4D2",
-  turquoise: "#7ED0DF",
-  asura: "#94DAD5",
-  orange: "#FF8B66",
-  brick: "#EB5933",
-  white: "#FFFFFF"
+	grey: "#EBE8E8",
+	oldpink: "#F0DBD8",
+	salmon: "#FFCAB8",
+	gold: "#F9C153",
+	purple: "#241F5D",
+	velvet: "#5353A2",
+	plum: "#6767AD",
+	emerald: "#78C4D2",
+	turquoise: "#7ED0DF",
+	asura: "#94DAD5",
+	orange: "#FF8B66",
+	brick: "#EB5933",
+	white: "#FFFFFF",
 };
 
 const size = {
-  small: "30px",
-  medium: "50px",
-  large: "200px",
-  xlarge: "300px"
+	small: "30px",
+	medium: "50px",
+	large: "200px",
+	xlarge: "300px",
 };
 
-const Container = styled.div`
-  .gm-ui-hover-effect {
-    opacity: 0;
-  }
-`;
-
-export const ItineraryMap = () => {
-  return (
-    <>
-      <Container>
-        <GoogleMapProvider>
-          <StandaloneAutocomplete
-            id="departure"
-            placeholder="Adresse de départ"
-            style={{
-              lineHeight: "1.2em",
-              width: "250px",
-              borderTop: "none",
-              borderLeft: "none",
-              borderRight: "none",
-              borderBottom: "1px solid #6767AD",
-              fontSize: "1.3em",
-              outline: "none",
-              padding: "20px",
-              margin: "10px",
-              background: "rgba(255, 255, 255, 0)",
-              color: "#241F5D"
-            }}
-          />
-
-          <StandaloneAutocomplete
-            id="arrival"
-            placeholder="Adresse d'arrivée"
-            style={{
-              lineHeight: "1.2em",
-              width: "250px",
-              borderTop: "none",
-              borderLeft: "none",
-              borderRight: "none",
-              borderBottom: "1px solid #6767AD",
-              fontSize: "1.3em",
-              outline: "none",
-              padding: "20px",
-              margin: "10px",
-              background: "rgba(255, 255, 255, 0)",
-              color: "#241F5D"
-            }}
-          />
-          <Datepicker />
-          <ButtonStyle label="VALIDER" />
-          <MapBox
-            apiKey={secrets.apiKey}
-            regionParam="FR"
-            languageParam="FR"
-            opts={{
-              center: { lat: 48.9333, lng: 2.3667 },
-              zoom: 12
-            }}
-            style={{
-              height: "70vh",
-              width: "100%",
-              margin: "0"
-            }}
-            useDrawing
-            useGeometry
-            usePlaces
-            useVisualization
-            useGoogleAPI
-            onCenterChanged={() => {
-              console.log("The center of the map has changed.");
-            }}
-          />
-          <Marker
-            id="marker"
-            opts={{
-              draggable: false,
-              label: "Départ",
-              position: { lat: 48.9333, lng: 2.3667 }
-            }}
-          />
-          <Marker
-            id="marker2"
-            opts={{
-              draggable: false,
-              label: "Arrivée",
-              position: { lat: 48.95, lng: 2.3167 }
-            }}
-          />
-          <InfoWindow anchorId="marker" opts={{}} visible>
-            <button
-              onClick={() => {
-                alert("trouvez les taxis");
-              }}
-            >
-              <CoDriving size={size.small} color={color.purple} /> <h2>42€</h2>
-            </button>
-          </InfoWindow>
-          <Polyline
-            id="polyline"
-            opts={{
-              path: [
-                { lat: 48.9333, lng: 2.3667 },
-                { lat: 48.9167, lng: 2.3833 },
-                { lat: 48.9, lng: 2.3333 },
-                { lat: 48.95, lng: 2.3167 }
-              ],
-              strokeColor: "orange"
-            }}
-          />
-          <OverlayView position={{ lat: 48.91, lng: 2.3667 }}>
-            <Taxi size={size.medium} color={color.purple} />
-            <h2>48€</h2>
-          </OverlayView>
-        </GoogleMapProvider>
-
-        <CardList />
-      </Container>
-    </>
-  );
+export const ItineraryMap = (props) => {
+	const [center, itineraryMapActions] = useGlobal(
+		(state) => state.itineraryMap.center,
+		(actions) => actions.itineraryMapActions,
+	);
+	const [itineraryArrival, arrivalActions] = useGlobal(
+		(state) => state.itinerary.arrival,
+		(actions) => actions.arrivalActions,
+	);
+	const [itineraryDeparture, departureActions] = useGlobal(
+		(state) => state.itinerary.departure,
+		(actions) => actions.departureActions,
+	);
+	return (
+		<>
+			<GoogleMapProvider>
+				<MapBox
+					apiKey={secrets.apiKey}
+					regionParam="FR"
+					languageParam="FR"
+					opts={{
+						center,
+						zoom: 9,
+					}}
+					style={{
+						height: "70vh",
+						width: "100%",
+						margin: "0",
+					}}
+					useDrawing
+					useGeometry
+					usePlaces
+					useVisualization
+					useGoogleAPI
+					onCenterChanged={() => {
+						console.log("The center of the map has changed.");
+					}}
+				/>
+				<Marker
+					id="marker"
+					opts={{
+						draggable: false,
+						label: "Départ",
+						position: {
+							lat: itineraryDeparture.lat,
+							lng: itineraryDeparture.lng,
+						},
+					}}
+				/>
+				<Marker
+					id="marker2"
+					opts={{
+						draggable: false,
+						label: "Arrivée",
+						position: { lat: itineraryArrival.lat, lng: itineraryArrival.lng },
+					}}
+				/>
+				<Polyline
+					id="polyline"
+					opts={{
+						path: [
+							{ lat: itineraryDeparture.lat, lng: itineraryDeparture.lng },
+							{ lat: itineraryArrival.lat, lng: itineraryArrival.lng },
+						],
+						strokeColor: "orange",
+					}}
+				/>
+			</GoogleMapProvider>
+		</>
+	);
 };
 export default ItineraryMap;
