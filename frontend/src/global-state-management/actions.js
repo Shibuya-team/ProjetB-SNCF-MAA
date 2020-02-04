a3ee41ba1e42b425a69d6729356810a4218fc81d
AudCor <clapyourhands4ever@gmail.com>
2020-02-04 11:34:03 +0100

    Corrections suite aux reviews

--- frontend/src/global-state-management/actions.js
+++ frontend/src/global-state-management/actions.js
@@ -229,9 +229,16 @@ export const validFormTravelActions = {
 
     const loopResultsFromAPI = async () => {
       return await axios
-        .get(
-          `http://localhost:5000/search/itinerary?destLat=${store.state.infosToAPIMaaS.destination.lat}&destLng=${store.state.infosToAPIMaaS.destination.lng}&oriLat=${store.state.infosToAPIMaaS.origin.lat}&oriLng=${store.state.infosToAPIMaaS.origin.lng}&searchDate=${store.state.infosToAPIMaaS.searchDate}&searchId=${store.state.itineraryDataFromMaaS.searchId}`
-        )
+        .get("http://localhost:5000/search/itinerary", {
+          params: {
+            destLat: `${store.state.infosToAPIMaaS.destination.lat}`,
+            destLng: `${store.state.infosToAPIMaaS.destination.lng}`,
+            oriLat: `${store.state.infosToAPIMaaS.origin.lat}`,
+            oriLng: `${store.state.infosToAPIMaaS.origin.lng}`,
+            searchDate: `${store.state.infosToAPIMaaS.searchDate}`,
+            searchId: `${store.state.itineraryDataFromMaaS.searchId}`
+          }
+        })
         .then(res => {
           if (store.state.itineraryDataFromMaaS.searchId === "") {
             store.setState({
