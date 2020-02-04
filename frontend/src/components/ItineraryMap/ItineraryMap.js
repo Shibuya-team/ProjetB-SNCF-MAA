232366b16731443186567903252402a863178d89
Camara Hadibéré <hadibere@gmail.com>
2020-02-04 08:11:56 +0100

    modif front

--- frontend/src/components/ItineraryMap/ItineraryMap.js
+++ frontend/src/components/ItineraryMap/ItineraryMap.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  GoogleMapProvider,
-  MapBox,
-  Marker,
-  Polyline
+	GoogleMapProvider,
+	MapBox,
+	Marker,
+	Polyline,
 } from "@googlemap-react/core";
 import Taxi from "../../images/icones/Mapicones/Taxi";
 import CoDriving from "../../images/icones/Mapicones/CoDriving";
@@ -33,7 +33,6 @@ const size = {
 	xlarge: "300px",
 };
 
-<<<<<<< HEAD
 export const ItineraryMap = (props) => {
 	const [center, itineraryMapActions] = useGlobal(
 		(state) => state.itineraryMap.center,
@@ -56,7 +55,7 @@ export const ItineraryMap = (props) => {
 					languageParam="FR"
 					opts={{
 						center,
-						zoom: 9,
+						zoom: 12,
 					}}
 					style={{
 						height: "70vh",
@@ -104,77 +103,5 @@ export const ItineraryMap = (props) => {
 			</GoogleMapProvider>
 		</>
 	);
-=======
-export const ItineraryMap = props => {
-  const [center, itineraryMapActions] = useGlobal(
-    state => state.itineraryMap.center,
-    actions => actions.itineraryMapActions
-  );
-  const [itineraryArrival, arrivalActions] = useGlobal(
-    state => state.itinerary.arrival,
-    actions => actions.arrivalActions
-  );
-  const [itineraryDeparture, departureActions] = useGlobal(
-    state => state.itinerary.departure,
-    actions => actions.departureActions
-  );
-  return (
-    <>
-      <GoogleMapProvider>
-        <MapBox
-          apiKey={secrets.apiKey}
-          regionParam="FR"
-          languageParam="FR"
-          opts={{
-            center,
-            zoom: 12
-          }}
-          style={{
-            height: "70vh",
-            width: "100%",
-            margin: "0"
-          }}
-          useDrawing
-          useGeometry
-          usePlaces
-          useVisualization
-          useGoogleAPI
-          onCenterChanged={() => {
-            console.log("The center of the map has changed.");
-          }}
-        />
-        <Marker
-          id="marker"
-          opts={{
-            draggable: false,
-            label: "Départ",
-            position: {
-              lat: itineraryDeparture.lat,
-              lng: itineraryDeparture.lng
-            }
-          }}
-        />
-        <Marker
-          id="marker2"
-          opts={{
-            draggable: false,
-            label: "Arrivée",
-            position: { lat: itineraryArrival.lat, lng: itineraryArrival.lng }
-          }}
-        />
-        <Polyline
-          id="polyline"
-          opts={{
-            path: [
-              { lat: itineraryDeparture.lat, lng: itineraryDeparture.lng },
-              { lat: itineraryArrival.lat, lng: itineraryArrival.lng }
-            ],
-            strokeColor: "orange"
-          }}
-        />
-      </GoogleMapProvider>
-    </>
-  );
->>>>>>> 2782cdbed28451b46057c2b88d598d26327954b4
 };
 export default ItineraryMap;
